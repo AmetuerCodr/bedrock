470ef0941440cb7f973c18c7ad2ca6a1b3385839
Pseudo_Supreme <gadgetguy49@gmail.com>
2026-05-10 07:26:32 -0400

    added reqwest

--- src/compiler/src/main.rs
+++ src/compiler/src/main.rs
@@ -1,17 +1,29 @@
 use serde_json::{Result, Value};
+
 use std::collections::HashMap;
 
-fn script_analyzer() {
+async fn script_analyzer() -> std::result::Result<(), Box<dyn std::error::Error>> {
     // analyzes script to find moments for visuals (lotties)
     // {
     // use rust reqwest to fetch data.json file
-    // 
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {body:?}");
+    Ok(())
+
+
+
+
+    //
     // use gemini api to analyze script to find moments that should be visuals
-    // 
+    //
     // returns json only, no commentary
-    // 
-    // example: 
-    // 
+    //
+    // example:
+    //
     // [
     //   {
     //     "index": 0,
@@ -24,7 +36,7 @@ fn script_analyzer() {
 
     // }
 
-    todo!()
+    // todo!()
 }
 
 fn animation_spec() {
@@ -36,33 +48,34 @@ fn lottie_compiler() {
     // maps keywords to valid lottie json
     todo!()
 }
+#[tokio::main]
+async fn main() -> Result<()> {
+    script_analyzer().await;
+    // let data = r#"{
+    //   "v": "5.5.7",
+    //   "fr": 30,
+    //   "ip": 0,
+    //   "op": 90,
+    //   "w": 500,
+    //   "h": 500,
+    //   "nm": "Example",
+    //   "ddd": 0,
+    //   "assets": [],
+    //   "layers": [
+    //     {
+    //       "ddd": 0,
+    //       "ind": 1,
+    //       "ty": 4,
+    //       "nm": "Circle",
+    //       "ks": {  },
+    //       "shapes": [ ]
+    //     }
+    //   ]
+    // }"#;
 
-fn main() -> Result<()> {
-    let data = r#"{
-      "v": "5.5.7",
-      "fr": 30,
-      "ip": 0,
-      "op": 90,
-      "w": 500,
-      "h": 500,
-      "nm": "Example",
-      "ddd": 0,
-      "assets": [],
-      "layers": [
-        {
-          "ddd": 0,
-          "ind": 1,
-          "ty": 4,
-          "nm": "Circle",
-          "ks": {  },
-          "shapes": [ ]
-        }
-      ]
-    }"#;
+    // let my_map: HashMap<String, serde_json::Value> = serde_json::from_str(data)?;
 
-    let my_map: HashMap<String, serde_json::Value> = serde_json::from_str(data)?;
-
-    println!("{:?}", my_map.get("layers").expect("layers does not exist"));
+    // println!("{:?}", my_map.get("layers").expect("layers does not exist"));
     Ok(())
     // map.insert("name", "John");
     // map.insert("city", "New York");
