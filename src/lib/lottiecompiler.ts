2382ad38a54e3392b56e4376825ec5638fd0b0eb
Pseudo_Supreme <gadgetguy49@gmail.com>
2026-05-11 20:28:28 -0400

    writing to file lottiemoments

--- src/lib/lottiecompiler.ts
+++ src/lib/lottiecompiler.ts
@@ -1,10 +1,12 @@
 import { resolve } from "node:path";
 import { VisualMomentArraySchema, type VisualMoment } from "./schema";
+import { appendFile } from "node:fs/promises";
 
 export async function runLottieCompiler(
   videoData: unknown,
 ): Promise<VisualMoment[]> {
   const binPath = resolve(import.meta.dir, "compiler");
+
   const proc = Bun.spawn([binPath], {
     stdin: "pipe",
     stdout: "pipe",
@@ -19,6 +21,8 @@ export async function runLottieCompiler(
     proc.exited,
   ]);
 
+  // TODO: if the error code is 503, switch models
+
   if (exitCode !== 0) {
     throw new Error(`compiler exited with code ${exitCode}`);
   }
@@ -31,4 +35,8 @@ if (import.meta.main) {
   const data = await Bun.file("./public/data.json").json();
   const moments = await runLottieCompiler(data);
   console.log(JSON.stringify(moments, null, 2));
+  await Bun.write(
+    "./public/lottiemoments.json",
+    JSON.stringify(moments, null, 2),
+  );
 }
